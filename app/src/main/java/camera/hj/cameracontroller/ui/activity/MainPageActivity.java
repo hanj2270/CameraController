17f5a354982c7d5395b8d0dba78372752f002b3e
hjc2270 <hjc2270@163.com>
2017-12-21 18:07:38 +0800

    增加activity

---
+++ app/src/main/java/camera/hj/cameracontroller/ui/activity/MainPageActivity.java
@@ -0,0 +1,10 @@
+package camera.hj.cameracontroller.ui.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by NC040 on 2017/12/21.
+ */
+
+public class MainPageActivity extends FragmentActivity {
+}
---
+++ app/src/main/java/camera/hj/cameracontroller/ui/activity/WelcomeActivity.java
@@ -0,0 +1,8 @@
+package camera.hj.cameracontroller.ui.activity;
+
+/**
+ * Created by NC040 on 2017/12/21.
+ */
+
+public class WelcomeActivity {
+}
